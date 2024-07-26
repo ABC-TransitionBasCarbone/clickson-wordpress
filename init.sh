6879d6f98fb5ed700de5edeb5edba27b8e9ce44d
EL HOUSSINE AIT EZOUINE <eaitezouine@norsys.fr>
2024-07-26 17:23:06 +0100

    fix database and composer.lock file

--- init.sh
+++ init.sh
@@ -9,5 +9,7 @@ echo ">>Installation in dev environement"
 docker build -t harbor.norsys-afrique.ma/clickson/clickson-wp-dev:latest docker --no-cache
 docker-compose up -d
 
+echo ">> Importing the initial database..."
+docker exec -i clickson-wordpress_db_1 mysql -uroot -proot clickson_wp_db > database/backup-wp-clickson.sql
 echo ">> fin"
 
